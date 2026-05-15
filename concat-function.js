720132de06805309e5001e71d10bd156dd524813
abhi-kachare <abhishekkachare100@gmail.com>
2026-05-15 22:39:35 +0530

    new file for learning the js

---
+++ concat-function.js
@@ -0,0 +1,11 @@
+//function declrations 
+function concat(str1 , str2){
+    return str1 +str2;
+}
+
+
+//function call and exqution 
+console.log(concat("abhi" , "kachare"));
+console.log(concat("age" , "23"));
+
+
